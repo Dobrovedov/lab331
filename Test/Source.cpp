f88973f49196d6a82890369d23a9849961175c68
Armund <erm_alex@list.ru>
2018-11-30 02:39:10 +0300

    Second Alex

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -4,12 +4,15 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include "LipkinForever.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello, Mr Lipkin!" << endl;
+	LipkinForever Lpp;
+	Lpp.Print_the_best();
 
 	_getch();
 	return 0;
