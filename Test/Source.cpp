b9ee6c8b6f608299e2511902fe5503c1a065a3ad
Dobrovedov <alexnemerovskii@gmail.com>
2018-11-30 02:14:22 +0300

    First

---
+++ Test/Source.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, Mr Lipkin!" << endl;
+
+	_getch();
+	return 0;
+}
