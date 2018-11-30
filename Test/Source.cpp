2335a554d258ba789231ffc56f23e5ba0972e914
Dobrovedov <alexnemerovskii@gmail.com>
2018-11-30 06:13:19 +0300

    10.1

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <string>
 #include "LipkinForever.h"
-
+#include "sm15.h"
 using namespace std;
 
 int main()
---
+++ Test/sm15.h
@@ -0,0 +1,8 @@
+#pragma once
+class sm15
+{
+public:
+	sm15();
+	~sm15();
+};
+
